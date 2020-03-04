e93e4431c32d70758ed14c2c58e7e8c498b6b961
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-03-04 13:03:50 -0500

    Created reusable counter component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Count } from './reusable/Count';
 
 export default class App extends React.Component {
   render() {
     return(
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Count count={0} />
       </View>
     );
   }
