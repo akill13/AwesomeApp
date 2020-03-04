d69a1de13c7f77f1e162db4e39c891e9128c9703
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-03-04 13:18:09 -0500

    Converted fuction component to class based component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Count } from './reusable/Count';
+import CustomCount from './reusable/Count';
 
 export default class App extends React.Component {
   render() {
     return(
       <View style={styles.container}>
-        <Count count={0} />
+        <CustomCount count={0} />
       </View>
     );
   }
--- reusable/Count.js
+++ reusable/Count.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import {StyleSheet, Text} from 'react-native'
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
     text: {fontSize: 72},
 })
-
-export const num = 50
-
-const Count = props => (
-<Text style={styles.text}>{props.count}</Text>
-)
+class Count extends React.Component {
+    static propTypes = {
+        count: PropTypes.number.isRequired,
+    }
+    render() {
+        return (
+            <Text style={styles.text}>{this.props.count}</Text>
+        )
+    }
+}
 
 export default Count;
