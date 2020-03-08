1c136ee882e0bbdd31b97264f39a9731cccd2a20
Abdul Halimi <abdulhalimi13@yahoo.com>
2020-03-07 23:56:07 -0500

    Setting up logic for contacts view and model

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import CustomCount from './reusable/Count';
 
 export default class App extends React.Component {
+  state = {
+    showContacts: false
+  }
+
+  toggleContacts = () => {
+    this.setState(prevState => ({showContacts: !prevState}))
+  }
+
   render() {
     return(
       <View style={styles.container}>
         <CustomCount count={0} />
+        <Button title='toggle contacts' onPress={this.toggleContacts} />
+        <ScrollView>
+          <Text>Filler Text</Text>
+        </ScrollView>
       </View>
     );
   }
